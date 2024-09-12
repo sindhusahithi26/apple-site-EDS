012d7d946e9b9af75e8142b35bf04b93df7568ca
sindhusahithi26 <sindhusahithi2623@gmail.com>
2024-09-12 19:59:24 +0530

    modal

--- blocks/banner/banner.js
+++ blocks/banner/banner.js
@@ -1,15 +1,8 @@
 
-
-
 export default function decorate(block) {
-  // Select all <h2> elements
   const headings = document.querySelectorAll("h2");
-
   headings.forEach((heading) => {
-    // Remove the 'id' attribute if it exists
     heading.removeAttribute("id");
-
-
     heading.classList.add("break-heading");
   });
 }
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -53,7 +53,17 @@ function buildAutoBlocks(main) {
     console.error('Auto Blocking failed', error);
   }
 }
+function autolinkModals(element) {
+  element.addEventListener('click', async (e) => {
+    const origin = e.target.closest('a');
 
+    if (origin && origin.href && origin.href.includes('/modals/')) {
+      e.preventDefault();
+      const { openModal } = await import(`${window.hlx.codeBasePath}/blocks/modal/modal.js`);
+      openModal(origin.href);
+    }
+  });
+}
 /**
  * Decorates the main element.
  * @param {Element} main The main element
@@ -99,6 +109,7 @@ async function loadEager(doc) {
  * @param {Element} doc The container element
  */
 async function loadLazy(doc) {
+  autolinkModals(doc);
   const main = doc.querySelector('main');
   await loadSections(main);
 
